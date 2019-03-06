f676f3cb1f018ddfa19572f30510bae818b2da6e
Jonnathan Astroz <jonathanastroz@gmail.com>
2019-03-06 07:04:50 -0500

    fin .Net, inicio Guia Interfaz, Actualización SO

---
+++ 4NetStandardDemo/UWPApp/UWPDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPApp
+{
+    class UWPDialog : NETStandardProject.IDialog
+    {
+        public async void Show(string message)
+        {
+            var Dialog =
+                new Windows.UI.Popups.MessageDialog(message);
+            await Dialog.ShowAsync();
+        }
+    }
+
+}
