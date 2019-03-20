961c785fbbfc38620528f6fc036e626341af3d8b
Jonnathan Astroz <jonathanastroz@gmail.com>
2019-03-19 20:46:09 -0500

    Creación de CDos Eliminación de HelloAttachedPropierties

---
+++ CDos/CDos/CDos/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CDos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void PracticaUno(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Practica1.MainPage());
+        }
+    }
+}
---
+++ CDos/CDos/CDos/Practica1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CDos.Practica1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async private void PracticaGrid(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Practica1.Ejercicio1());
+        }
+
+        async private void PracticaProp(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Practica1.Ejercicio2());
+        }
+    }
+}
